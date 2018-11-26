78bb66b92ba4a4d3d2f3ade088e6f9254b7c0b8b
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-11-26 01:05:54 +0100

    panel login

--- js/script.js
+++ js/script.js
@@ -26,13 +26,52 @@ $(document).ready(function(){
 		$('#panel').load('./panels/panelLogin.html');
 	});
 	
-	$('#btnLogin').on("click", function(){
-		var user = $('#login_usuario').val();
-		var psw = $('#login_psw').val();
+	$('#btnLogin').click(function(){
+		let user = $('#usuarioInput');
+		let psw = $('#claveInput');
+
+		let check = comprobarLogin(user, psw);
+
+		if(check){
+            $.ajax({
+                url: 'http://fenw.etsisi.upm.es:5555/users/login?username='+user.val()+'&password='+psw.val(),
+                async: true,
+                type: 'GET',
+                success: function (data, texStatus, request) {
+                    window.document.cookie = 'tokenAPI='+request.getResponseHeader('Authorization')+'; path=/ ;expires= 31 Dec 2050 23:59:59 GMT;';
+                    window.document.location.href = 'index.html';
+                },
+                error: function (e) {
+                    console.log('ERROR: ' + e);
+                }
+            });
+		}
+
+
 	});
 	
 	function comprobarRegistro() {
 		var usuario = $('#usuarioInput').val();
 		
     }
+
+    function comprobarLogin(user, psw){
+		let check = true;
+
+        if(user.val() === ''){
+            user.addClass('error');
+            check = false;
+        }else{
+            user.removeClass('error');
+        }
+
+        if(psw.val() === ''){
+            psw.addClass('error');
+            check = false;
+        }else{
+            psw.removeClass('error');
+        }
+
+        return check;
+	}
 });
